9046978e717d06a388a5b736d807724899272025
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-07-01 15:21:59 -0400

    scaling the circle to test sfml

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    
+    sf::Clock clock;
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    shape.setOrigin({shape.getRadius(), shape.getRadius()});
+    shape.setPosition({ window.getSize().x / 2.f, window.getSize().y / 2.f });
 
     while (window.isOpen())
     {
@@ -15,7 +21,12 @@ int main()
         }
 
         window.clear();
+
+        float scalefactor = std::sin(clock.getElapsedTime().asSeconds());
+        std::cout << std::sin(clock.getElapsedTime().asSeconds());
+        shape.setScale({ scalefactor, scalefactor });
         window.draw(shape);
+
         window.display();
     }
 }
